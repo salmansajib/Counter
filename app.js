27ee798eb63c4b7a5fc44c5d7f7ec9b01636a216
Salman-sajib <sfsajib@gmail.com>
2023-02-25 21:15:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// set initial value for the counter
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    const styles = event.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "limegreen";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
